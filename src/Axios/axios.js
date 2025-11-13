47ab01316acdcbf7bd2aebe2e80df8276f41e45a
Arsathali-L <arsathliakath092003@gmail.com>
2025-11-13 23:50:14 +0530

    to-do front

--- src/Axios/axios.js
+++ src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"https://to-do-backend-1-a2zk.onrender.com"
+    baseURL:"https://to-do-backend-1-a2zk.onrender.com/api"
 })
 export default instance
